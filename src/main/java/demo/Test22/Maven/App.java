c1896331fbbb66df127c2fadd3d846491e1408bd
Ashok_Nagappan <jjancychristy1988@gmail.com>
2023-05-04 11:37:03 -0400

    code by y

--- src/main/java/demo/Test22/Maven/App.java
+++ src/main/java/demo/Test22/Maven/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );//code by X
+        System.out.println( "Hello World!" );//code by Y
+        
     }
 }
