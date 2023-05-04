896a77e8299be293c23de99a4ebfea34e2da26d1
Ashok_Nagappan <jjancychristy1988@gmail.com>
2023-05-04 11:15:52 -0400

    commit by x

---
+++ src/main/java/demo/Test22/Maven/App.java
@@ -0,0 +1,13 @@
+package demo.Test22.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );//code by X
+    }
+}
